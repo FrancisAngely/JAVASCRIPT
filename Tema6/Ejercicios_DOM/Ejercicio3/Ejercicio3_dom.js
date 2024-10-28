f93166a685d916be2b4664688ea2f25c30aef02a
Francis Gamboa <francis.a.gamboag@gmail.com>
2024-10-28 13:19:55 +0100

    Actualizacion ejercicios tema 6

---
+++ Tema6/Ejercicios_DOM/Ejercicio3/Ejercicio3_dom.js
@@ -0,0 +1,29 @@
+document.getElementById("addItemButton").addEventListener("click", function () {
+  const itemInput = document.getElementById("itemInput");
+  const itemSelect = document.getElementById("itemSelect");
+
+  const newItem = itemInput.value.trim();
+
+  if (newItem) {
+    const option = document.createElement("option");
+    option.value = newItem;
+    option.textContent = newItem;
+    itemSelect.appendChild(option);
+    itemInput.value = ""; // Limpiar el cuadro de texto
+  } else {
+    alert("Por favor, introduce un ítem.");
+  }
+});
+
+document
+  .getElementById("removeItemButton")
+  .addEventListener("click", function () {
+    const itemSelect = document.getElementById("itemSelect");
+
+    if (itemSelect.options.length > 1) {
+      // Verificamos que haya ítems para eliminar
+      itemSelect.remove(itemSelect.options.length - 1); // Eliminar el último ítem
+    } else {
+      alert("No hay ítems para eliminar.");
+    }
+  });

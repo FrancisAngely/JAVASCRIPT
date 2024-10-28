ce2a06a79033cbf8f9953c5d9ca09abf7d327857
francis <francis.a.gamboag@gmail.com>
2024-10-28 18:50:13 +0100

    Actualice el ejercicio 1

--- Tema6/Ejercicios_DOM/Ejercicio1/Ejercicio1_dom.js
+++ Tema6/Ejercicios_DOM/Ejercicio1/Ejercicio1_dom.js
@@ -1,8 +1,23 @@
 const elemento=document.getElementById("opcion"); 
 const cuadro=document.getElementById("cuadoTexto");
 
-elemento.addEventListener("click", function(){
+elemento.addEventListener("change", function(){
+    const valor=elemento.options[elemento.selectedIndex]; 
 
-    const seleccion=elemento.value; 
-    cuadro.value=seleccion; 
+    console.log(valor.textContent);
+    cuadro.value=valor.textContent; 
+    //const seleccion=elemento.value; 
 }); 
+
+/*
+const colores = document.getElementById("colores");
+const txt = document.getElementById("txt");
+
+colores.addEventListener("change", function (){
+
+const valor = colores.options[colores.selectedIndex];
+console.log(valor.textContent);
+txt.value = valor.textContent;
+})
+
+*/

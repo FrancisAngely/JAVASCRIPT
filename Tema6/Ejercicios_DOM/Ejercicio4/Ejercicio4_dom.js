f8b50da9e3b932aacf34aa8db96ab23866d82df7
francis <francis.a.gamboag@gmail.com>
2024-11-11 17:39:57 +0100

    respaldo

--- Tema6/Ejercicios_DOM/Ejercicio4/Ejercicio4_dom.js
+++ Tema6/Ejercicios_DOM/Ejercicio4/Ejercicio4_dom.js
@@ -1,33 +1,35 @@
 const colores = document.getElementById("colores");
 const txt = document.getElementById("txt");
-const boton_añadir = document.getElementById("añadir");
+const boton_anadir = document.getElementById("anadir");
 const boton_eliminar = document.getElementById("eliminar");
 
-
 function comprobarItem(valor) {
-for (let color of colores.options) {
-if (color.textContent === valor) {
-alert("No se puede añadir ese valor, valor duplicado");
-return false;
+    for (let color of colores.options) {
+        if (color.textContent === valor) {
+            alert("No se puede añadir ese valor, valor duplicado");
+            return false;
+        }
+    }
+    return true;
 }
-}
-return true;
-}
-boton_añadir.addEventListener("click", function () {
-const valor = txt.value;
-console.log(valor);
 
-if (comprobarItem(valor) && valor !== "") {
-const option = document.createElement("option");
-option.textContent = valor;
-option.value = valor;
-colores.appendChild(option);
+boton_anadir.addEventListener("click", function () {
+    const valor = txt.value;
+    console.log(valor);
 
-}
-txt.value = "";
+    if (comprobarItem(valor) && valor !== "") {
+        const option = document.createElement("option");
+        option.textContent = valor;
+        option.value = valor;
+        colores.appendChild(option);
+    }
+    txt.value = "";
 });
 
 boton_eliminar.addEventListener("click", function () {
-//colores.remove(colores.options.length - 1);
-colores.removeChild(colores.options[colores.length - 1]);
-}); 
+    if (colores.length > 0) { // Asegúrate de que hay opciones para eliminar
+        colores.removeChild(colores.options[colores.length - 1]);
+    } else {
+        alert("No hay opciones para eliminar");
+    }
+});
